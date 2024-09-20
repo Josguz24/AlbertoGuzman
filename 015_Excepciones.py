bdff9cccdfbfa80b2455bd6a1b53df8323fe6d1c
Jalberto <136873132+Josguz24@users.noreply.github.com>
2024-09-19 20:03:33 -0600

    015_Excepciones.py

--- 015_Excepciones.py
+++ 015_Excepciones.py
@@ -2,13 +2,18 @@
 def dividir_numeros(a, b):
     try:
         resultado = a / b
+        #EN CASO DE PRESENTAR UN ERROR EJECUTAR CUALQUIERA DE LOS EXCEPT
     except ZeroDivisionError:
+         #SI SE DIVIDE ENTRE 0
         print("Error: No se puede dividir entre cero.")
     except TypeError:
+        #SI SE DIVIDE ENTRE LETRAS U OTRO
         print("Error: Los valores deben ser números.")
     else:
+        #MOSTRAR EL RESULTADO
         print(f"El resultado de {a} dividido entre {b} es: {resultado}")
     finally:
+        # SE IMPRIME SI O SI
         print("Fin del cálculo.")
 
 # Pruebas de la función
